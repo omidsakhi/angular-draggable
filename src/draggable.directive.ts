77beda681b60e21c45e317ff5ee3b565320f932e
Omid Sakhi <omid.sakhi@gmail.com>
2017-06-01 13:09:32 -0400

    Modified for our purpose

--- src/draggable.directive.ts
+++ src/draggable.directive.ts
@@ -1,68 +1,55 @@
-import { Directive, ElementRef, Renderer, OnDestroy, OnInit, AfterViewInit, Input } from '@angular/core';
-
+import { Directive, ElementRef, Renderer, OnDestroy, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Directive({
-  selector: '[draggable]',
+  selector: '[app-draggable]',
   host: {
     '(dragstart)': 'onDragStart($event)',
     '(dragend)': 'onDragEnd($event)',
     '(drag)': 'onDrag($event)'
   }
 })
-export class DraggableDirective implements OnDestroy, OnInit, AfterViewInit {
-  private Δx: number = 0;
-  private Δy: number = 0;
-  
-  private canDrag:boolean = true;
-  
-  @Input('draggable')
-  set draggable(val:any){
-    if(val === undefined || val === null || val === '' ) return;
-    this.canDrag = !!val;
-  }
-  private mustBePosition: Array<string> = ['absolute', 'fixed', 'relative'];
+export class DraggableDirective implements OnDestroy, OnInit {
+
+  /**
+   * A signal that is emitted when drag ends
+   */
+  @Output() dragEnded = new EventEmitter<ElementRef>();
+
+  private Δx = 0;
+  private Δy = 0;
+
   constructor(
     private el: ElementRef, private renderer: Renderer
-  ) {
-    
-  }
-  
-  ngOnInit(): void {
+  ) {}
+
+  public ngOnInit(): void {
     this.renderer.setElementAttribute(this.el.nativeElement, 'draggable', 'true');
   }
-  ngAfterViewInit(){
-    try {
-      let position = window.getComputedStyle(this.el.nativeElement).position;
-      if (this.mustBePosition.indexOf(position) === -1) {
-        console.warn( this.el.nativeElement, 'Must be having position attribute set to ' + this.mustBePosition.join('|'));
-      }
-    } catch (ex) {
-      console.error(ex);
-    }
-  }
-  ngOnDestroy(): void {
-    this.renderer.setElementAttribute(this.el.nativeElement, 'draggable', 'false');
-  }
-  
+
   onDragStart(event: MouseEvent) {
     this.Δx = event.x - this.el.nativeElement.offsetLeft;
     this.Δy = event.y - this.el.nativeElement.offsetTop;
   }
 
   onDrag(event: MouseEvent) {
-    this.doTranslation(event.x, event.y);
+    const x = event.x;
+    const y = event.y;
+
+    if (!x || !y) {
+      return;
+    }
+    this.renderer.setElementStyle(this.el.nativeElement, 'top', (y - this.Δy) + 'px');
+    this.renderer.setElementStyle(this.el.nativeElement, 'left', (x - this.Δx) + 'px');
   }
 
   onDragEnd(event: MouseEvent) {
     this.Δx = 0;
     this.Δy = 0;
+    this.dragEnded.emit(this.el);
   }
 
-  doTranslation(x: number, y: number) {
-    if (!x || !y) return;
-    this.renderer.setElementStyle(this.el.nativeElement, 'top', (y - this.Δy) + 'px');
-    this.renderer.setElementStyle(this.el.nativeElement, 'left', (x - this.Δx) + 'px');
+  public ngOnDestroy(): void {
+    this.renderer.setElementAttribute(this.el.nativeElement, 'draggable', 'false');
   }
-  
 
 }
